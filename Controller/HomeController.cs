e2832d821d18142d72a8fa27fe61337e993c1b40
6145207 <ashwinidhumal0810@gmail.com>
2023-12-14 14:20:57 +0530

    add files

---
+++ Controller/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+ using Microsoft.Extensions.Logging;
+    
+
+    namespace AzureKeyVaulWebApp.Controllers
+    {
+        public class HomeController : Controller
+        {
+            private readonly ILogger<HomeController> _logger;
+            private readonly IConfiguration _configuration;
+
+            public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+            {
+                _logger = logger;
+                _configuration = configuration;
+            }
+
+            public IActionResult Index()
+            {
+                // Read the secret from the Azure Key Vault
+                string secretValue = _configuration["ConString"];
+                //string secretValue1 = _configuration["FeatureMeshOffline"];
+
+            // Pass the secret value to the view
+            ViewBag.SecretValue = secretValue;
+           // ViewBag.SecretValue = secretValue1;
+
+
+            return View();
+            }
+
+            // Other action methods and methods of HomeController
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,49 @@
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+//app.Run();
+using Azure.Identity;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Azure Key Vault configuration
+builder.Configuration.AddAzureKeyVault(
+    new Uri("https://featuremeshkeyvault.vault.azure.net/"),
+    
+    new DefaultAzureCredential());
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
